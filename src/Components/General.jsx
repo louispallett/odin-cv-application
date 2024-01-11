42f2c0a849900abe38ffb29e8c622db127bbe204
louispallett <louis.pallett@outlook.com>
2024-01-11 21:05:00 +0000

    Add Components for InputLine in General

--- src/Components/General.jsx
+++ src/Components/General.jsx
@@ -1,14 +1,34 @@
 import { useState } from "react";
-import InputLine from "./InputLine";
 
-export default function General() {
+export default function General() {    
     return (
         <section>
             <fieldset>
                 <legend>General</legend>
                 <InputLine label="First Name" _id="first_name"/>
                 <InputLine label="Last Name" _id="last_name" />
+                <InputLine label="Email" type="email" _id="email" />
+                <InputLine label="Number" type="tel" _id="number" />
             </fieldset>
         </section>
     )
 }
+
+function InputLine({label, type, _id}) {
+    const [editStatus, setEditStatus] = useState(true);
+
+    return (
+        <div>
+            <label htmlFor={_id}>{label}</label>
+            <input 
+                type={type} 
+                id={_id}
+                className={editStatus ? "edit" : "noEdit"}
+            />
+        </div>
+    );
+}
+
+InputLine.defaultProps = {
+    type: "text"
+};
