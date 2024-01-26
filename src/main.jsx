431ba65ecdd80a0c52cbdc093f99ce4a3f57112c
Louis Pallett <louis.pallett@outlook.com>
2024-01-26 18:03:30 +0000

    Comments on Netlify deployment

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.scss';
 
-
 import General from './Components/General.jsx';
 import Education from './Components/Education.jsx';
 import Practical from './Components/Practical.jsx';
+
+/*
+====================================
+Deploying Vite/React on Netlify 
+====================================
+
+You can deploy Vite/React projects on Netlify very easily by following the UI instructions
+(just linking it to the GitHub repo). 
+
+BUT, it won't deploy the main.jsx file (and in turn the CSS/SCSS files). To fix this, make sure that the:
+
+  1.  Build command is set to "npm run build"
+  2.  Publish directory is set to "dist".
+
+You can do this on set up or go into the "deploy" settings.
+*/
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
